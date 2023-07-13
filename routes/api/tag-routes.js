4fc545f8499c85b019cec83ce23a752a690ffd01
afrique18 <vincentv1225@gmail.com>
2023-07-13 12:36:22 -0400

    Added GET route for Tad id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,14 +1,21 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag, Category } = require('../../models');
 
 // The `/api/tags` endpoint
 
 router.get('/', async(req, res) => {
   try {
-    const tagData = await Tag.findAll({
-      include: [{ model: Product}],
+    const productData = await Product.findAll({
+      include: [{ model: Category }, 
+        {
+          model: Tag,
+          through: {
+            attributes: ['id', 'product_id', 'tag_id'],
+          },
+        },
+      ],
     });
-    res.status(200).json(tagData);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json({ message: 'Tags not found!' });
   }
@@ -16,7 +23,27 @@ router.get('/', async(req, res) => {
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, 
+        {
+          model: Tag,
+          through: {
+            model: ProductTag,
+            attributes: ['id', 'product_id', 'tag_id'],
+          },
+        },
+      ],
+    });
+    if (!productData) { 
+      res.status(400).json({ message: 'No tag found with this id'});
+      return;
+    } 
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
